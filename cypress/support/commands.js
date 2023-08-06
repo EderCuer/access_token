02c9c3e047694340b64e29f5bcc869d960a72b2a
EderCuer <ederscuer@hotmail.com>
2023-08-06 20:32:39 -0300

    Adicionando imagens

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker'
 
 Cypress.Commands.add('geraAccessToken', () => {
+  // Utilizando o faker para simular a geração de token
   const token = faker.internet.password({ length: 50 })
   return token
 })
